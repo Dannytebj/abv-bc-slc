c33498208cd5b98b9e2d4ad9a32b4ec1cf109df2
Dannyb <dannyb4me@cbn.gov>
2017-05-16 05:51:13 +0100

    Add Comments to code

--- app/library.js
+++ app/library.js
@@ -1,4 +1,5 @@
 function isPrimeNumber(number) {
+  //this function checks for prime numbers
   let isPrime = true;
   Array.apply(null, Array(number)).forEach((_, index) => {
     const currentNumber = index + 2;
@@ -10,6 +11,7 @@ function isPrimeNumber(number) {
 }
 
 function getPrimes(n) {
+  //this function gets all prime numbers within n by checking against isPrimeNumber
    if (!Number.isInteger(n)) {
     return [];
   }
